4839da0b5fac87c6d09b587f7f6317c6afc7e0e9
Thor Håkon Bredesen <thor.hakon.bredesen@gmail.com>
2015-08-13 22:52:05 +0200

    add comments to end divs in footer

--- thea_app/footer.php
+++ thea_app/footer.php
@@ -1,4 +1,5 @@
-			</div>
-		</div>
+			</div> <?php // Ends <div class="ui thirteen wide column margin-top-30 fade-in" id="maincontent"> ?>
+		</div> <?php // Ends <div class="ui grid container"> ?>
+		</div> <?php // Ends <div class="pusher ?>
 	</body>
 </html>
