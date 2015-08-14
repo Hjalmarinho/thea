49759b3056608390d7812cb10649e089e8bd7b9c
Hjalmarinho <molnes.oystein@gmail.com>
2015-08-14 11:07:17 +0200

    Show if a participant is accreditated or not

--- thea_app/js/accreditation.js
+++ thea_app/js/accreditation.js
@@ -6,61 +6,63 @@ $(document).ready(function(){
 	});
 
 
-	apiGetParticipants(displayAccreditations);
+	apiGetParticipants(displayParticipants);
 
 });
 
+//Populate table-body with all participants
+function displayParticipants(participants){
+	var participants_body = ('#participants_body')
+	$.each(participants, function (i, participant){
 
-function displayAccreditations(accreditations){
-	var accreditationsbody = ('#accreditations_body')
-	$.each(accreditations, function (i, accreditant){
-		var first_name = accreditant.person.first_name;
-		var last_name = accreditant.person.last_name;
-		var club = accreditant.club.club_name;
-		var time_registrated = new Date(accreditant.time_registrated).customFormat("#DD# #MMM# #YYYY#, kl. #hhh#.#mm#.#ss#");
-		var entry_id = accreditant.entry_id;
-
-		console.log(accreditant);
-
-		var tablerow = 		'<tr onclick="accreditantClicked('+entry_id+')">'+ 
-								'<td>' + first_name + '</a></td>'+
-								'<td>' + last_name + '</td>'+
-								'<td>' + club + '</td>'+
+		var tablerow = 		'<tr onclick="participantClicked('+participant.entry_id+')">'+ 
+								'<td>' + participant.person.first_name + '</a></td>'+
+								'<td>' + participant.person.last_name + '</td>'+
+								'<td>' + participant.club.club_name + '</td>'+
 							'</tr>';
-		$(accreditationsbody).append(tablerow);
+		$(participants_body).append(tablerow);
 	});
 
 
 };	
 
 //Called when a participant in the table is clicked
-function accreditantClicked(entry_id){
+function participantClicked(entry_id){
 	if(entry_id){
-		apiGetParticipant(entry_id, displayAccreditant);
+		//Get participant from API and display it's data in the participant_card
+		apiGetParticipant(entry_id, displayParticipant);
 	}
 }
 
 // Display a participant on the card
-function displayAccreditant(accreditant){
-	$('#accreditant_card').show();
-	$('#accreditant_card').val(accreditant.entry_id);
-	$('#card_name').text(accreditant.person.first_name+' '+accreditant.person.last_name);
+function displayParticipant(participant){
+	$('#participant_card').show();
+	$('#participant_card').val(participant.entry_id);
+	$('#card_name').text(participant.person.first_name+' '+participant.person.last_name);
 
-	var time_registrated = new Date(accreditant.time_registrated).customFormat("#DD# #MMM# #YYYY#, kl. #hhh#.#mm#.#ss#")
-	$('#time_registrated').text(time_registrated);
+	var time_registrated = new Date(participant.time_registrated).customFormat("#DD# #MMM# #YYYY#, kl. #hhh#.#mm#.#ss#")
+	$('#card_time_registrated').text(time_registrated);
 
-	$('#comment').text('Dette er en hardkodet kommentar om denne deltakeren');
+	//Display green check-icon if participant has been accreditated
+	if(participant.accreditated){
+		$('#card_accreditated_mark').show();
+	}
+	else{
+		$('#card_accreditated_mark').hide();
+	}
+
+	$('#card_comment').text('Dette er en hardkodet kommentar om denne deltakeren');
 };
 
 
-//Accreditate participant by getting the entry_id from the value-attribute of the card
+//Accreditate participant by getting the entry_id from the value-attribute of the participant_card
 function accreditateParticipant(){
-	var entry_id = $( '#accreditant_card' ).val();
+	var entry_id = $( '#participant_card' ).val();
 	var jsonData = {"accreditated": true};
 	apiPutAccreditation(entry_id, jsonData, participantAccreditated);
 }
 
 //Callback function when a participant has been accreditated
 function participantAccreditated(accreditation){
-	console.log('Participant '+accreditation.entry_id+' was successfully accreditated');
+	$('#accreditated_mark').show();
 }
