23bd504ec84a1091e7e2d28ce587649980497cba
Thor Håkon Bredesen <thor.hakon.bredesen@gmail.com>
2015-11-27 16:50:31 +0100

    Add posibility for showing sports in participants.php

--- thea_app/js/participants.js
+++ thea_app/js/participants.js
@@ -25,6 +25,16 @@ function displayParticipants(participants){
 			default:
 				return true;
 		}
+
+		/* If NSI wants to show all participant sports for a participant
+		var sports = []
+		if (participant.exercises.length > 0){
+			for (i = 0; i < participant.exercises.length; i++){
+				sports.push(participant.exercises[i].exercise.exercise_description)
+			}
+		}
+		*/
+
 		var first_name = participant.person.first_name
 		var last_name = participant.person.last_name
 		var gender = customGenderFormat(participant.person.gender)
@@ -33,6 +43,10 @@ function displayParticipants(participants){
 		var email = participant.person.email
 		var time_registrated = parseDateString(participant.time_registrated).customFormat("#DD# #MMM# #YYYY#, kl. #hhh#.#mm#.#ss#")
 		var entry_id = participant.entry_id
+
+		if (participant.exercises.length == 0){
+			console.log(first_name + ' ' + last_name)
+		}
 
 		var tablerow = '<tr ' + cssClass + '><td><a href="./participant.php?entry_id=' + entry_id +'">' + first_name + '</a></td><td>' + last_name + '</td><td>' + 
 		gender + '</td><td>' + club + '</td><td>' + phone + '</td><td><a href="mailto:' + email + '">' + email + '</a></td><td>' + 
