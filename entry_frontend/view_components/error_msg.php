4ae48109ef9f6591ed3ba7abb15716f890b7197f
Thea <thea@thea.no>
2015-12-07 17:27:56 +0100

    Add error message

---
+++ entry_frontend/view_components/error_msg.php
@@ -0,0 +1,6 @@
+<div class="ui warning message">
+  <div class="header">
+    Problemer med betaling
+  </div>
+  Vi har for tiden problemer med betaling fra VISA-kort, og jobber med å finne en løsning på dette. Vi beklager ulempene dette medfører.
+</div>
