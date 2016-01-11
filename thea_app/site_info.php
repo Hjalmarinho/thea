a5259fdddc63e16f690a477510d20c245d65825f
Erik Frøseth <erik@froseth.org>
2016-01-11 20:44:03 +0100

    Clean up site-info-things

---
+++ thea_app/site_info.php
@@ -0,0 +1,6 @@
+<?php
+
+// Read the root url from settings file.
+$content = file_get_contents(__DIR__ . "/../settings.json");
+$jsonObject = json_decode($content);
+define('ROOT_URL', $jsonObject->frontend->thea_app_base_url);
