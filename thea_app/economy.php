5dd967b4867336c71fa1803ddb5eab0620b21622
Erik Froseth <erik.froseth@oracle.com>
2017-03-01 13:34:28 +0100

    economy
    - Remove footer.php
    - Remove header.php
    - Upgrade to latest jquery, semantic-ui and bignumber
    - Use datatables for fast searching and rendering

--- thea_app/economy.php
+++ thea_app/economy.php
@@ -1,53 +1,92 @@
-<?php require 'header.php';?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8" />
+    <title>Thea 2.0</title>
 
-<div class="ui items">
-    <div class="header">
-      <!-- <i class="large sidebar icon" id="menubutton"></i> -->
-      <h1>Økonomi</h1>
-      <p id="summary"></p>
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.9/semantic.min.css" />
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/datatables/1.10.13/css/dataTables.semanticui.min.css" />
+    <link rel="stylesheet" type="text/css" href="css/style.css" />
+
+    <script>
+    <?php
+      // Include shared code
+      require_once(__DIR__ . "/site_info.php");
+      require_once(__DIR__ . "/../shared/js/shared.js");
+      require_once(__DIR__ . "/../shared/js/api_handler.js");
+    ?>
+    </script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.9/semantic.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/datatables/1.10.13/js/jquery.dataTables.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/datatables/1.10.13/js/dataTables.semanticui.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/bignumber.js/4.0.0/bignumber.min.js"></script>
+
+    <script src="js/shared.js"></script>
+    <script src="js/economy.js"></script>
+  </head>
+  <body>
+    <div class="ui sidebar inverted vertical menu">
+      <?php require 'include/menu.php';?>
     </div>
-</div>
+    <div class="pusher">
+      <div class="ui grid">
+        <div class="computer only three wide column" >
+          <div class="ui vertical inverted menu" >
+            <?php require 'include/menu.php';?>
+          </div>
+        </div>
 
-<div>
-  <div id="transactionsLoader" class="ui active inverted dimmer">
-    <div class="ui large text loader">Henter transaksjoner...</div>
-  </div>
-  <table class="ui selectable stackable sortable celled table">
-    <thead>
-      <tr>
-        <th class="ascending right aligned">Ordrenr.</th>
-        <th>Transaksjons-ID</th>
-        <th>Fornavn</th>
-        <th>Etternavn</th>
-        <th class="right aligned">Sum</th>
-        <th class="right aligned">Refundert</th>
-        <th class="right aligned">Akkumulert</th>
-        <th>Ordrestatus</th>
-        <th>Dato</th>
-    </tr>
-    </thead>
-    <tbody id="transactions">
-    </tbody>
-  </table>
-</div>
+        <div class="ui thirteen wide column margin-top-30 fade-in" id="context">
+          <div class="ui items">
+              <div class="header">
+                <!-- <i class="large sidebar icon" id="menubutton"></i> -->
+                <h1>Økonomi</h1>
+                <p id="summary"></p>
+              </div>
+          </div>
 
-<div class="ui modal" id="payment-log">
-  <div id="payment-loader" class="ui active inverted dimmer">
-    <div class="ui large text loader">Henter...</div>
-  </div>
-  <div class="header">
-    Betalingslogg
-  </div>
-  <div class="content">
-    <div class="ui yellow message">Alle pengesummer her er multiplisert med 100. Dvs, når det står "55000", betyr det "550.00".</div>
-    <pre id="payment-log-content">
-    </pre>
-  </div>
-  <div class="actions">
-    <div class="ui green button close">Ok</div>
-  </div>
-</div>
+          <div>
+            <div id="transactionsLoader" class="ui active inverted dimmer">
+              <div class="ui large text loader">Henter transaksjoner...</div>
+            </div>
+            <table class="ui selectable stackable sortable celled table">
+              <thead>
+                <tr>
+                  <th class="ascending right aligned">Ordrenr.</th>
+                  <th>Transaksjons-ID</th>
+                  <th>Fornavn</th>
+                  <th>Etternavn</th>
+                  <th class="right aligned">Sum</th>
+                  <th class="right aligned">Refundert</th>
+                  <th class="right aligned">Akkumulert</th>
+                  <th>Ordrestatus</th>
+                  <th>Dato</th>
+              </tr>
+              </thead>
+              <tbody>
+              </tbody>
+            </table>
+          </div>
 
-<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/bignumber.js/3.0.0/bignumber.js"></script>
-
-<?php require 'footer.php';?>
+          <div class="ui modal" id="payment-log">
+            <div id="payment-loader" class="ui active inverted dimmer">
+              <div class="ui large text loader">Henter...</div>
+            </div>
+            <div class="header">
+              Betalingslogg
+            </div>
+            <div class="content">
+              <div class="ui yellow message">Alle pengesummer her er multiplisert med 100. Dvs, når det står "55000", betyr det "550.00".</div>
+              <pre id="payment-log-content">
+              </pre>
+            </div>
+            <div class="actions">
+              <div class="ui green ok button">Ok</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </body>
+</html>
--- thea_app/js/economy.js
+++ thea_app/js/economy.js
@@ -2,12 +2,15 @@
 
 var event_id = sessionStorage.getItem('event_id');
 
-$(document).ready(function() {
+$(document).ready(function()
+{
   var request = apiGetTransactionsSummary(displayTransactions, handleError, event_id);
   $.when(request).always(function() { removeLoader(); });
 });
 
-function displayTransactions(transactions) {
+
+function displayTransactions(transactions)
+{
   sortArrayByNumber(transactions, 'order_number');
 
   var format = {
@@ -21,81 +24,110 @@ function displayTransactions(transactions) {
   BigNumber.config({ FORMAT: format })
 
   var accumulated = new BigNumber(0);
-  $.each(transactions, function (i, transaction) {
-    var tablerow;
-
-    if (transaction.order_status == "COMPLETED")
-      tablerow = '<tr class="positive">';
-    else
-      tablerow = '<tr>';
-
-    tablerow = tablerow + '<td class="right aligned">';
-    tablerow = tablerow + transaction.order_number;
-    tablerow = tablerow + '</td>';
-
-    tablerow = tablerow + '<td onclick="showPaymentLog(this, \'' + transaction.transaction_id + '\');" style="cursor:pointer;color:#5b9aff;">';
-    tablerow = tablerow + transaction.transaction_id;
-    tablerow = tablerow + '</td>';
-
-    tablerow = tablerow + '<td onclick="window.location.href =\'./participant.php?entry_id=' + transaction.entry_id + '\';" style="cursor:pointer;color:#5b9aff;">';
-    tablerow = tablerow + transaction.first_name;
-    tablerow = tablerow + '</td>';
-
-    tablerow = tablerow + '<td>';
-    tablerow = tablerow + transaction.last_name;
-    tablerow = tablerow + '</td>';
-
-    var order_amount = new BigNumber(transaction.order_amount);
-    tablerow = tablerow + '<td class="right aligned">';
-    tablerow = tablerow + order_amount.toFormat(2);
-    tablerow = tablerow + '</td>';
-
-    var amount_refunded = new BigNumber(transaction.amount_refunded);
-    tablerow = tablerow + '<td class="right aligned">';
-    tablerow = tablerow + amount_refunded.toFormat(2);
-    tablerow = tablerow + '</td>';
-
+  for (var i = 0; i < transactions.length; ++i)
+  {
+    var transaction = transactions[i];
     if (transaction.order_status == "COMPLETED")
     {
-      accumulated = accumulated.plus(order_amount);
-      accumulated = accumulated.minus(amount_refunded);
+      accumulated = accumulated.plus(transaction.order_amount);
+      accumulated = accumulated.minus(transaction.amount_refunded);
     }
-    tablerow = tablerow + '<td class="right aligned">';
-    tablerow = tablerow + accumulated.toFormat(2);
-    tablerow = tablerow + '</td>';
 
-    var order_status_str;
-    if (transaction.order_status == "COMPLETED")
-      order_status_str = "Fullført";
-    else if (transaction.order_status == "CREATED")
-      order_status_str = "Opprettet";
-    else if (transaction.order_status == "INITIALIZED")
-      order_status_str = "Påbegynt";
+    transaction.accumulated = accumulated;
+  }
 
-    tablerow = tablerow + '<td>';
-    tablerow = tablerow + order_status_str
-    tablerow = tablerow + '</td>';
-
-    var time_registered = parseDateString(transaction.time_registered);
-    var time_registered_str = time_registered.customFormat('#DD# #MMM# #YYYY#, kl. #hhh#.#mm#.#ss#');
-    tablerow = tablerow + '<td data-sort-value="' + time_registered.getTime() + '">';
-    tablerow = tablerow + time_registered_str;
-    tablerow = tablerow + '</td>';
-
-    tablerow = tablerow + '</tr>';
-
-    $('#transactions').append(tablerow);
-  });
-
+  $('table').DataTable({
+    'columns': [
+      { 'data': 'order_number', 'className': 'right aligned' },
+      {
+        'data': 'transaction_id',
+        'createdCell': function (td, cellData, rowData, row, col)
+        {
+          $(td).attr('onclick', 'showPaymentLog(this, \'' + rowData.transaction_id + '\');');
+          $(td).css('cursor', 'pointer');
+          $(td).css('color', '#5b9aff');
+        }
+      },
+      {
+        'data': 'first_name',
+        'createdCell': function (td, cellData, rowData, row, col)
+        {
+          $(td).attr('onclick', 'window.location.href="./participant.php?entry_id=' + rowData.entry_id + '";');
+          $(td).css('cursor', 'pointer');
+          $(td).css('color', '#5b9aff');
+        }
+      },
+      { 'data': 'last_name' },
+      { 'data': 'order_amount', 'render': money_column_render, 'className': 'right aligned' },
+      { 'data': 'amount_refunded', 'render': money_column_render, 'className': 'right aligned' },
+      { 'data': 'accumulated', 'render': money_column_render, 'className': 'right aligned' },
+      { 'data': 'order_status', 'render': order_status_column_render },
+      { 'data': 'time_registered', 'render': time_registered_column_render }
+    ],
+    'pagingType': 'numbers',
+    'info': false,
+    'language':
+    {
+      'search': 'Søk',
+      'lengthMenu': 'Vis _MENU_ transaksjoner'
+    },
+    'lengthMenu': [ [10, 25, 50, -1], [10, 25, 50, 'Alle'] ],
+    'order': [[0, 'desc']],
+    'createdRow': function ( row, data, index )
+    {
+      if (data.order_status === 'COMPLETED')
+        $(row).addClass('positive');
+    },
+  }).rows.add(transactions).column(1).search('^(?!\s*$).+', true).draw();
 }
 
-function removeLoader() {
+function time_registered_column_render( data, type, full, meta )
+{
+  var date = parseDateString(data);
+  if (type === 'sort')
+    return date.getTime();
+  else
+    return date.customFormat('#DD# #MMM# #YYYY#, kl. #hhh#.#mm#.#ss#');
+}
+
+function money_column_render( data, type, full, meta )
+{
+  if (type === 'display')
+  {
+    var tmp = new BigNumber(data);
+    return tmp.toFormat(2);
+  }
+  return data;
+}
+
+
+function order_status_column_render( data, type, full, meta )
+{
+  switch (data)
+  {
+    case 'COMPLETED':
+      return 'Fullført';
+    case 'CREATED':
+      return 'Opprettet';
+    case 'INITIALIZED':
+      return 'Påbegynt';
+    default:
+      return '';
+  }
+}
+
+
+function removeLoader()
+{
   $('#transactionsLoader').removeClass('active');
 }
 
-function handleError(errorMsg) {
+
+function handleError(errorMsg)
+{
   console.log(errorMsg);
 }
+
 
 function showPaymentLog(sender, transactionId)
 {
