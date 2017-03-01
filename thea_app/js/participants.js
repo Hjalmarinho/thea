5d5d5d4593fb054fba491cb7e5bc74eb0cfe582a
Erik Froseth <erik.froseth@oracle.com>
2017-03-01 14:45:16 +0100

    participants
    - Remove header.php
    - Remove footer.php
    - Upgrade to latest semantic-ui and jquery
    - Use datatables for fast searching and rendering

--- thea_app/js/participants.js
+++ thea_app/js/participants.js
@@ -7,96 +7,114 @@ $(document).ready(function() {
   $.when(request).always(function() { removeLoader(); });
 });
 
-function displayParticipants(participants) {
-  sortArrayByNumberDesc(participants, 'entry_id');
 
-  var numCancelled = 0;
-  var numConfirmed = 0;
-
-  $.each(participants, function (i, participant) {
-    var cssClass = "";
-    switch (participant.status) {
-      case REGISTRATION_CONFIRMED:
-        numConfirmed++;
-        break;
-      case REGISTRATION_CANCELLED:
-        cssClass = "error";
-        numCancelled++;
-        break;
-      default:
-        return true;
-    }
-
-    var first_name = participant.first_name;
-    var last_name = participant.last_name;
-    var gender = customGenderFormat(participant.gender);
-    var club = participant.club_name;
-    var phone = participant.phone;
-    var email = participant.email;
-    var time_registrated = parseDateString(participant.time_registrated);
-    var time_registrated_str = time_registrated.customFormat('#DD# #MMM# #YYYY#, kl. #hhh#.#mm#.#ss#');
-    var entry_id = participant.entry_id;
-    var ticket_type = 'Ukjent';
-
-    if (participant.ticket_type == 'PARTICIPANT')
-      ticket_type = 'Deltaker';
-    else if (participant.ticket_type == 'SPECTATOR')
-      ticket_type = 'Tilskuer';
-
-    var tablerow = '<tr class="' + cssClass + '">';
-
-    tablerow = tablerow + '<td onclick="window.location.href =\'./participant.php?entry_id=' + entry_id + '\';" style="cursor:pointer;">';
-    tablerow = tablerow + first_name;
-    tablerow = tablerow + '</td>';
-
-    tablerow = tablerow + '<td>';
-    tablerow = tablerow + last_name;
-    tablerow = tablerow + '</td>';
-
-    tablerow = tablerow + '<td>';
-    tablerow = tablerow + gender;
-    tablerow = tablerow + '</td>';
-
-    tablerow = tablerow + '<td>';
-    tablerow = tablerow + club;
-    tablerow = tablerow + '</td>';
-
-    tablerow = tablerow + '<td>';
-    tablerow = tablerow + phone;
-    tablerow = tablerow + '</td>';
-
-    tablerow = tablerow + '<td>';
-    tablerow = tablerow + '<a href="mailto:' + email + '">' + email + '</a>';
-    tablerow = tablerow + '</td>';
-
-    tablerow = tablerow + '<td>';
-    tablerow = tablerow + ticket_type;
-    tablerow = tablerow + '</td>';
-
-    tablerow = tablerow + '<td data-sort-value="' + time_registrated.getTime() + '">';
-    tablerow = tablerow + time_registrated_str;
-    tablerow = tablerow + '</td>';
-
-    tablerow = tablerow + '<td onclick="getReceipt(' + entry_id + ');">';
-    tablerow = tablerow + '<i class="download blue icon"></i>';
-    tablerow = tablerow + '</td>';
-
-    tablerow = tablerow + '</tr>';
-
-    $('#participants').append(tablerow);
-  });
-
-  $('#summary').html(numConfirmed + ' påmeldte, ' + numCancelled + ' kansellerte.');
+function gender_column_render( data, type, full, meta )
+{
+  return customGenderFormat(data);
 }
 
-function removeLoader() {
+
+function time_registered_column_render( data, type, full, meta )
+{
+  var date = parseDateString(data);
+  if (type === 'sort')
+    return date.getTime();
+  else
+    return date.customFormat('#DD# #MMM# #YYYY#, kl. #hhh#.#mm#.#ss#');
+}
+
+
+function ticket_type_column_render(data, type, full, meta)
+{
+  switch (data)
+  {
+    case 'PARTICIPANT':
+      return 'Deltaker';
+    case 'SPECTATOR':
+      return 'Tilskuer';
+    default:
+      return 'Ukjent';
+  }
+}
+
+
+function receipt_column_render( data, type, full, meta )
+{
+  return '<i class="download blue icon"></i>';
+}
+
+
+function displayParticipants(participants)
+{
+  var table = $('table').DataTable({
+    'pagingType': 'numbers',
+    'info': false,
+    'language':
+    {
+      'search': 'Søk',
+      'zeroRecords': 'Ingen treff',
+      'lengthMenu': 'Vis _MENU_ deltakere'
+    },
+    'columns':
+    [
+      {
+        'data': 'first_name',
+        'createdCell': function (td, cellData, rowData, row, col)
+        {
+          $(td).attr('onclick', 'window.location.href="./participant.php?entry_id=' + rowData.entry_id + '";');
+          $(td).css('cursor', 'pointer');
+          $(td).css('color', '#5b9aff');
+        }
+      },
+      { 'data': 'last_name' },
+      { 'data': 'gender', 'render': gender_column_render },
+      { 'data': 'club_name' },
+      { 'data': 'phone' },
+      { 'data': 'email' },
+      { 'data': 'ticket_type', 'render': ticket_type_column_render },
+      { 'data': 'time_registrated', 'render': time_registered_column_render },
+      {
+        'data': null,
+        'render': receipt_column_render,
+        'createdCell': function (td, cellData, rowData, row, col)
+        {
+          $(td).attr('onclick', 'getReceipt(' + rowData.entry_id + ');');
+          $(td).css('cursor', 'pointer');
+          $(td).css('color', '#5b9aff');
+        }
+      }
+    ],
+    'order': [[7, 'desc']],
+    'createdRow': function ( row, data, index )
+    {
+      switch (data.status)
+      {
+        case REGISTRATION_CANCELLED:
+          $(row).addClass('error');
+          break;
+      }
+    }
+  }).rows.add(participants).draw();
+
+  var num_confimed = table.rows(function ( idx, data, node ) { return data.status == REGISTRATION_CONFIRMED; }).count();
+  var num_cancelled = table.rows(function ( idx, data, node ) { return data.status == REGISTRATION_CANCELLED; }).count();
+  $('#summary').html(num_confimed + ' påmeldte, ' + num_cancelled + ' kansellerte.');
+}
+
+
+function removeLoader()
+{
   $('#participantsLoader').remove();
 }
 
-function handleError(errorMsg) {
+
+function handleError(errorMsg)
+{
   console.log(errorMsg);
 }
 
-function addNewParticipant() {
-  var request = apiPostNewParticipant(function(data) {location.href = './participant.php?first=true&entry_id=' + data.entry_id;}, handleError, "", event_id);
+
+function addNewParticipant()
+{
+  var request = apiPostNewParticipant(function(data) {location.href = './participant.php?first=true&entry_id=' + data.entry_id;}, handleError, '', event_id);
 }
